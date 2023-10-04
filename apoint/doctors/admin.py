5f18c3ca6aa01ae0b5b6cea94c285c37fc78d1a0
Damir Nureev <damirn29@gmail.com>
2023-10-04 16:06:01 +0300

    fix

--- apoint/doctors/admin.py
+++ apoint/doctors/admin.py
@@ -2,20 +2,36 @@ from django.contrib import admin
 from .models import Doctor, Schedule, Appointment, Specialization
 
 
+class AppointmentInline(admin.TabularInline):
+    model = Appointment
+    extra = 1
+
 @admin.register(Doctor)
 class ShowDoctors(admin.ModelAdmin):
     list_display = ['id', 'name', 'specialization']
     list_editable = ['name', 'specialization']
+    list_filter = ['specialization']
+    inlines = [AppointmentInline] #встроенное редактирование раcписания в таблице doctors
 
 @admin.register(Schedule)
 class ShowSchedule(admin.ModelAdmin):
     list_display = ['doctor', 'date', 'start_time', 'end_time', 'available_slots']
     list_editable = ['date', 'start_time', 'end_time', 'available_slots']
+    list_filter = ['doctor']
+    fieldsets = (
+        ('Doctor', {
+            'fields': ('doctor', 'available_slots')
+        }),
+        ('Date', {
+            'fields': ('date', 'start_time', 'end_time')
+        }),
+    )
 
 @admin.register(Appointment)
 class ShowAppointment(admin.ModelAdmin):
     list_display = ['id', 'patient', 'doctor']
     list_editable = ['patient', 'doctor']
+    list_filter = ['doctor']
 
 @admin.register(Specialization)
 class ShowSpecialization(admin.ModelAdmin):
